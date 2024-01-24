1858882c4115026805715e6223b2c7709d6167bf
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-01-24 09:56:20 +0300

    Update learning_script.py

--- learning_script.py
+++ learning_script.py
@@ -5,20 +5,19 @@ from dotenv import load_dotenv
 from google.cloud import dialogflow
 
 
-def open_learning_file():
+def open_learning_file(learning_file: str) -> dict:
     with open(learning_file, 'r', encoding="UTF-8") as file:
         data = json.load(file)
     return data
 
 
 def create_intent(
+        project_id: str,
         display_name: str,
         training_phrases_parts: list,
         message_texts: list
 ) -> None:
-    project_id = os.getenv("DIALOGFLOW_ID")
     intents_client = dialogflow.IntentsClient()
-
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
@@ -42,15 +41,21 @@ def create_intent(
     print("Intent created: {}".format(response))
 
 
-if __name__ == '__main__':
-    load_dotenv()
+def main():
+    project_id = os.getenv("DIALOGFLOW_ID")
     learning_file = os.getenv("LEARN_FILE_PATH")
-    data_learning_file = open_learning_file()
+    data_learning_file = open_learning_file(learning_file=learning_file)
     for learn_name, training_phrases in data_learning_file.items():
         training_phrases_parts = training_phrases['questions']
         message_texts = [training_phrases['answer']]
         create_intent(
+            project_id=project_id,
             display_name=learn_name,
             training_phrases_parts=training_phrases_parts,
             message_texts=message_texts
         )
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    main()
